5802bf95923f073c0f5f152c15d73ef360c3b53b
Jean-Charles JOREL <jeancharlesjorel@gmail.com>
2020-12-22 21:39:55 +0000

    (Improvment/Major) Introduce tool versionning.

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+VERSION=$1
+APP_NAME=ec2-spot-converter
+if [ -z "$VERSION" ] ; then
+	echo "Usage: $0 <version>" ; exit 1
+fi
+versionned_file="${APP_NAME}-${VERSION}"
+if [ -e "releases/${versionned_file}" ]; then
+	echo "Version ${VERSION} already exists!" ; exit 1
+fi
+echo ${VERSION} >VERSION.txt
+sed "s/::Version::/$VERSION/g" <src/${APP_NAME}.py | 
+	sed "s/::ReleaseDate::/$(date)/g" > releases/${versionned_file}
+chmod a+x releases/${versionned_file}
+ln -sf releases/${versionned_file} ${APP_NAME}
+ln -sf ${versionned_file} releases/${APP_NAME}-latest
+git add releases/${versionned_file} releases/latest
+git commit -m "Releasing ${APP_NAME} version $VERSION" releases/$VERSION releases/latest 
+git tag -a $VERSION -m "Releasing ${APP_NAME} version $VERSION"
+git push
+git push --tags
