2ce555eabf8558be89ac23b42f56b66c6cc89c54
Jean-Charles JOREL <jeancharlesjorel@gmail.com>
2020-12-23 09:58:52 +0000

    Updated sample output in RADME.md

--- src/ec2-spot-converter.py
+++ src/ec2-spot-converter.py
@@ -642,7 +642,7 @@ def reboot_if_needed():
             "Rebooted": False
             })
     if not args["reboot_if_needed"]:
-        return (True, f"It is recommend to reboot '{instance_id}'... Please specify --reboot-if-needed option next time.", {
+        return (True, f"It is recommended to reboot '{instance_id}' but --reboot-if-needed option not in the command line: Do nothing.", {
             "Rebooted": False
             })
 
